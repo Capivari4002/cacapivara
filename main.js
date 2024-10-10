d0c778eb4a1d014db43989bad66024e8616fd5fa
Capivari4002 <teixeira.rodrigues@escola.pr.gov.br>
2024-10-10 12:19:47 -0300

    dd

---
+++ main.js
@@ -0,0 +1,7 @@
+function criacartao (categoria,pergunta,resposta.)
+let container = document.getElementById(`container`);
+let cartao = document.createElement('article');
+cartao.className = 'cartao';
+cartao.innerHTML =`
+
+`
---
+++ perguntas.js
@@ -0,0 +1,137 @@
+criacartao(
+
+'história',
+'período em que Getúlio Vargas ficou no poder até que houvesse eleições para tornar legal seu governo',
+'governo provisório')
+
+
+criacartao (
+'história',
+'sigla símbolo dos Mártires do levante armado que tinha como objetivo depor Varga em 1932',
+'MMDC')
+
+
+criacartao (
+
+'história',
+'vice presidente que assumiu o governo após a morte de Vargas',
+'Café Filho',)
+
+
+criacartao(
+
+'história',
+'quando Vargas criou a CLT',
+'01 de maio de 1943',)
+
+
+criacartao(
+
+'português',
+'quais os temas abordados no naturalismo',
+'miséria, violência, crimes, patologia humana, sexualidade, adultério, dentre outros')
+
+
+criacartao(
+
+'sociologia',
+'o que é sociologia',
+'a sociedade é umas das ciências humanas que estuda a sociedade, ou seja, estuda o comportamento humanos em funçao do meio e os processos que interligam os indivíduos e associações, grupos e instituições')
+
+
+criacartao (
+
+'empreendedorismo',
+'Quais são os principais elementos que сомрõем um plano de negócio',
+'sumário executivo, descrição da empresa,análise de mercado, estratégia de marketing')
+
+
+criacartao (
+
+'biología',
+'Quais os princípios da teoria da evolução das espécies?',
+'Adaptação ao meio e Seleção natural')
+
+
+criacartao (
+
+'Biologia',
+'Principal componente químico do núcleo e dos cromossomos, formada de proteínas histonas associadas a molécula de DNA', 
+'Cromatina')
+
+
+criacartao (
+
+'Biologia',
+'Células que possuem apenas um conjunto cromossômico (n)',
+ 'Haplóide')
+
+
+criacartao (
+
+'Biologia',
+'Qual o nome da célula usada para promover a circulação de água e absorção de nutrientes nas esponjas',
+'Coanócito')
+
+
+criacartao(
+
+'Geografia',
+'Qual a definição de Geopolítica?',
+'é a área do conhecimento que estuda os estados que se relacionam entre si e as diversas estratégias adotadas para administrar seus territórios')
+
+ 
+
+criacartao(
+'Geografia',
+'Por que os conflitos geopolíticos aumentam?',
+'Devido a rivalidade entre as potências, as mudanças climáticas e tensões econômicas')
+
+
+criacartao(
+'Geografia',
+'Quais as consequências do processo de globalização para as indústrias?',
+'Gera um aumento da tecnologia e da complexidade de alguns produtos, levando a dispersão industrial')
+
+
+criacartao(
+
+'Educação financeira',
+' O que são impostos',
+'é um tributo que serve para custear o serviço público, sendo destinado aos serviços prestados à sociedade, como saúde, educação, segurança, infraestrutura e assistência social')
+
+
+criacartao(
+
+'Geografia',
+'Quais as áreas com maior concentração industrial atualmente?',
+'Canadá, Estados Unidos, Rússia, Japão, Índia, Nova Zelândia e Austrália')
+
+
+criacartao(
+
+'Educação Financeira',
+'Qual é a definição de inflação?',
+'Inflação é o aumento dos preços de bens e serviços')
+
+
+criacartao(
+
+'Educação Financeira',
+'O que significa a sigla INPC?',
+'índice Nacional de Preços ao Consumidor')
+
+
+criacartao(
+
+'Educação Financeira',
+'Qual a função do prefeito?',
+'administrar a cidade em que vive, cobrar impostos e taxas que, por sua vez, devem custear obras, serviços e políticas essenciais para a vida nas cidades')
+
+
+criacartao(
+
+'Empreendedorismo',
+'O que é a Matriz SWOT?',
+'é uma ferramenta estratégica de gestão usada para identificar os pontos fortes e fracos, tanto internos quanto externos, de uma empresa, pessoa, produto ou serviço'
+)
